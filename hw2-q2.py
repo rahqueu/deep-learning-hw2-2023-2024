c536cb0ef2e87aa86be7fdcab1316f5e370436c0
Inês Ji <inesji@tecnico.ulisboa.pt>
2024-01-06 22:27:14 +0000

    Added q3 and cleaned q2

--- hw2-q2.py
+++ hw2-q2.py
@@ -22,7 +22,6 @@ class CNN(nn.Module):
         self.no_maxpool = no_maxpool
         
         if not no_maxpool:
-            # Implementation for Q2.1
             self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
             #conv1, 8 output channels, kernel 3x3, stride 1, padding to preserve original image size
             #padding chosen due: stride 1, padding = (kernel - 1)/2 = (3-1)/2 = 1
@@ -30,30 +29,30 @@ class CNN(nn.Module):
         
             #conv2, 16 output channels, kernel 3x3, stride 1, padding 0
             self.conv2 = nn.Conv2d(8, 16, kernel_size = 3, padding = 0) 
+            
+            #input features = #output_channels x output_width x output_height
+            #output_width = (input_width + padding_right + padding_left - kernel_width) / stride + 1
+            #output_height = (input_height + padding_height_top + padding_height_bottom - kernel_height) / stride + 1
+            self.fc1_in_features = 16 * 6 * 6 
         else:
-            #conv1, 8 output channels, kernel 3x3, stride 1, padding to preserve original image size
-            #padding chosen due: stride 1, padding = (kernel - 1)/2 = (3-1)/2 = 1
+            #conv1, 8 output channels, kernel 3x3, stride 2, padding 1
             self.conv1 = nn.Conv2d(1, 8, kernel_size = 3, stride = 2, padding = 1)
         
-            #conv2, 16 output channels, kernel 3x3, stride 1, padding 0
+            #conv2, 16 output channels, kernel 3x3, stride 2, padding 0
             self.conv2 = nn.Conv2d(8, 16, kernel_size = 3, stride = 2, padding = 0) 
-            # Implementation for Q2.2
+            
+            #input features = #output_channels x output_width x output_height
+            #output_width = (input_width + padding_right + padding_left - kernel_width) / stride + 1
+            #output_height = (input_height + padding_height_top + padding_height_bottom - kernel_height) / stride + 1
+            self.fc1_in_features = 16 * 6 * 6
         
-        #input features = #output_channels x output_width x output_height
-        #output_width = (input_width + padding_right + padding_left - kernel_width) / stride + 1
-        #output_height = (input_height + padding_height_top + padding_height_bottom - kernel_height) / stride + 1
-        
-        self.fc1_in_features = 16 * 6 * 6 
         self.fc1 = nn.Linear(self.fc1_in_features, 320)
         self.fc2 = nn.Linear(320, 120)
         self.fc3 = nn.Linear(120, 4)
         self.drop = nn.Dropout(p=dropout_prob)
         
-        # Implementation for Q2.1 and Q2.2
-        #raise NotImplementedError
         
     def forward(self, x):
-        
         # input should be of shape [b, c, w, h]
         x = x.view(x.shape[0], 1, 28, 28)
           
@@ -129,10 +128,8 @@ def plot(epochs, plottable, ylabel='', name=''):
 
 
 def get_number_trainable_params(model):
-    #Number of trainable weights = Number of f ilters × ((kernel width × kernel height×number of channels) + bias)
     model_parameters_cnn = filter(lambda p: p.requires_grad, model.parameters())
     params_cnn = sum([np.prod(p.size()) for p in model_parameters_cnn])
-    ## TO IMPLEMENT - REPLACE return 0
     return params_cnn
 
 
@@ -144,12 +141,12 @@ def main():
     parser.add_argument('-batch_size', default=8, type=int,
                         help="Size of training batch.")
     parser.add_argument('-learning_rate', type=float, default=0.01,
-                        help="""Learning rate for parameter updates""")
+                        help="""Learning rate for parameter updates""") #0.1, 0.01, 0.001
     parser.add_argument('-l2_decay', type=float, default=0)
     parser.add_argument('-dropout', type=float, default=0.7)
     parser.add_argument('-optimizer',
                         choices=['sgd', 'adam'], default='sgd')
-    parser.add_argument('-no_maxpool', action='store_false')
+    parser.add_argument('-no_maxpool', action='store_true') #store_true for max_pooling, store_false otherwise
     
     opt = parser.parse_args()
 
